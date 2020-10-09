400a8858ab3aaf6853d64694b9e0f790f5923545
Or Goldfus <orgoldfus@Ors-MacBook-Pro-2.local>
2020-10-09 16:12:53 +0300

    basic login

--- src/index.js
+++ src/index.js
@@ -1,28 +1,41 @@
 const { CompanyTypes, createScraper } = require('israeli-bank-scrapers');
 const credentials = require('../credentials.json');
+const _ = require('lodash');
 
-(async function() {
+(async function () {
   try {
     // read documentation below for available options
     const options = {
-      companyId: CompanyTypes.leumi, 
-      startDate: new Date('2020-09-10'),
+      companyId: CompanyTypes.leumi,
+      startDate: new Date('2020-10-01'),
       combineInstallments: false,
-      showBrowser: false 
+      showBrowser: false
     };
 
     const scraper = createScraper(options);
     const scrapeResult = await scraper.scrape(credentials);
 
-    if (scrapeResult.success) {
-      scrapeResult.accounts.forEach((account) => {
-        console.log(`found ${account.txns.length} transactions for account number ${account.accountNumber}`);
-      });
-    }
-    else {
+    if (!scrapeResult.success) {
       throw new Error(scrapeResult.errorType);
     }
-  } catch(e) {
+
+    const account = scrapeResult.accounts[0];
+    const { income, expenses } = _.groupBy(account.txns, (stmnt) =>
+      stmnt.chargedAmount > 0 ? 'income' : 'expenses'
+    );
+
+    const totalIncome = _.sumBy(income, 'chargedAmount');
+    const totalExpenses = _.sumBy(expenses, 'chargedAmount');
+    const cashflow = totalExpenses + totalIncome;
+
+    console.log(JSON.stringify({
+      income,
+      expenses,
+      totalIncome,
+      totalExpenses,
+      cashflow
+    }, null, 2));
+  } catch (e) {
     console.error(`scraping failed for the following reason: ${e.message}`);
   }
 })();
